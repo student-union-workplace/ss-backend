2a8885b9dea33fae382f4c6e5e6a5121173132bb
flexiG <mister23krut@gmail.com>
2024-11-30 15:39:49 +0500

    добавлена авторизация

---
+++ server/src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './auth.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: AuthDto) {
+    return this.authService.signIn(signInDto.email, signInDto.password);
+  }
+}
---
+++ server/src/auth/auth.service.ts
@@ -0,0 +1,43 @@
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(
+    inputEmail: string,
+    inputPassword: string,
+  ): Promise<{ access_token: string }> {
+    const user = await this.usersService.findOneEmail(inputEmail);
+
+    if (!user) {
+      throw new NotFoundException(`Пользователь с ${inputEmail} не найден`);
+    }
+
+    const isPasswordValid = bcrypt.compareSync(inputPassword, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Введён неверный пароль');
+    }
+
+    const payload = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      department_id: user.department_id,
+      role: user.department_id,
+    };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
