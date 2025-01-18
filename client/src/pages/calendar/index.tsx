e49596d639c7c7084f4b15edbca2b49656ecb559
Alyona Filyaeva <alyonchikfilyaeva@yandex.ru>
2025-01-18 11:29:15 +0500

    add calendar

--- client/src/pages/calendar/index.tsx
+++ client/src/pages/calendar/index.tsx
@@ -1,3 +1,28 @@
+import {Box} from "@mui/material";
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
 export const CalendarPage = () => {
-    return <p>Это календарь</p>
+    return <Box className={'content'}
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    maxWidth: '1900px',
+                    minHeight: '100%',
+                    flexGrow: 1
+                }}>
+        <FullCalendar
+            plugins={[ dayGridPlugin ]}
+            initialView="dayGridMonth"
+            height={'35rem'}
+            locale={'ru'}
+            firstDay={1}
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true} 
+        />
+    </Box>
 }
